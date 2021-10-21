08e6d2bffc55818bb394be01b7729395afe7cd96
nesherson <nesad.omeragich27@gmail.com>
2021-10-21 18:52:49 +0200

    Add users controller and implement index action

---
+++ Appointify.Admin/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Appointify.Data;
+using Appointify.Admin.ViewModels.Users;
+
+namespace Appointify.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly DatabaseContext _dbContext;
+
+        public UsersController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var viewModel = new IndexViewModel
+            {
+                Users = await _dbContext.Users
+                .Where(u => u.Role != Data.Entities.Role.SuperAdministrator)
+                .Include(u => u.City)
+                .ToListAsync()
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
