1a105795d472ebff807be3bd8340557bf6a1a81a
nesherson <nesad.omeragich27@gmail.com>
2021-10-22 08:02:54 +0200

    Code refactor

--- Appointify.Admin/Utilities/Dropdown.cs
+++ Appointify.Admin/Utilities/Dropdown.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Appointify.Data;
 using Appointify.Data.Entities;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -8,20 +9,39 @@ namespace Appointify.Admin.Utilities
 {
     public class Dropdown
     {
-        private readonly DatabaseContext _databaseContext;
+        private readonly DatabaseContext _dbContext;
 
-        public Dropdown(DatabaseContext databaseContext)
+        public Dropdown(DatabaseContext dbContext)
         {
-            _databaseContext = databaseContext;
+            _dbContext = dbContext;
         }
 
         public async Task<SelectList> Countries(int? selectedId = null)
         {
-            var countries = await _databaseContext.Countries.ToListAsync();
+            var countries = await _dbContext.Countries.ToListAsync();
 
             var selectList = new SelectList(countries, nameof(Country.Id), nameof(Country.Name), selectedId);
 
             return selectList;
         }
+
+        public async Task<SelectList> Cities(int? selectedId = null)
+        {
+            var cities = await _dbContext.Cities.ToListAsync();
+
+            var selectList = new SelectList(cities, nameof(City.Id), nameof(City.Name), selectedId);
+
+            return selectList;
+        }
+
+        public async Task<SelectList> Companies(int? selectedId = null)
+        {
+            var companies = await _dbContext.Companies.ToListAsync();
+
+            var selectList = new SelectList(companies, nameof(Company.Id), nameof(Company.Name), selectedId);
+
+            return selectList;
+        }
+
     }
 }
