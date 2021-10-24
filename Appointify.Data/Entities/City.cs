aecbcb9c426c68e61839908d56a152a92075e8d4
nesherson <nesad.omeragich27@gmail.com>
2021-10-24 21:32:04 +0200

    Update entities

--- Appointify.Data/Entities/City.cs
+++ Appointify.Data/Entities/City.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; }
         public string PostalCode { get; set; }
+
         public int CountryId { get; set; }
         public Country Country { get; set; }
     }
--- Appointify.Data/Entities/User.cs
+++ Appointify.Data/Entities/User.cs
@@ -10,9 +10,13 @@ namespace Appointify.Data.Entities
         public string Username { get; set; }
         public string PasswordHash { get; set; }
         public string PasswordSalt { get; set; }
+
         public Role Role { get; set; }
+
         public int CityId { get; set; }
         public City City { get; set; }
+
+        public ICollection<Company> Companies { get; set; }
         public ICollection<Appointment> Appointments { get; set; }
     }
 }
