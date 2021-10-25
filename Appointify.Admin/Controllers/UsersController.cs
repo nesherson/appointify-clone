457f884d38ad7c50e73b7c50d933c4403fbf0b85
nesherson <nesad.omeragich27@gmail.com>
2021-10-25 07:47:50 +0200

    Implement add user functionality

--- Appointify.Admin/Controllers/UsersController.cs
+++ Appointify.Admin/Controllers/UsersController.cs
@@ -2,9 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Linq;
+using AutoMapper;
 
 using Appointify.Data;
 using Appointify.Admin.ViewModels.Users;
+using Appointify.Admin.Resources;
+using Appointify.Shared.Utilities;
+using Appointify.Data.Entities;
 
 namespace Appointify.Admin.Controllers
 {
@@ -12,10 +16,13 @@ namespace Appointify.Admin.Controllers
     {
 
         private readonly DatabaseContext _dbContext;
+        private readonly IMapper _mapper;
 
-        public UsersController(DatabaseContext dbContext)
+
+        public UsersController(DatabaseContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -42,8 +49,36 @@ namespace Appointify.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> AddUser(AddViewModel viewModel)
         {
+
+            if (viewModel.Username == null || viewModel.Email == null)
+            {
+                ModelState.AddModelError(string.Empty, Strings.AddUserInputRequired);
+                return View();
+            }
+
+            var userAlreadyExists = await _dbContext.Users.AnyAsync(u => u.Username == viewModel.Username || u.Email == viewModel.Email);
+            if (userAlreadyExists)
+            { 
+                ModelState.AddModelError(string.Empty, Strings.UserAlreadyExists);
+                return View();
+            }
+
+
+            var salt = Cryptography.Salt.Create();
+            var hash = Cryptography.Hash.Create(viewModel.Password, salt);
+
+            var user = new User
+            {
+                PasswordSalt = salt,
+                PasswordHash = hash
+            };
+
+            _mapper.Map(viewModel, user);
+
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
             return View();
-            
         }
 
         public async Task<JsonResult> GetCompanies()
