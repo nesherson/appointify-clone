5eb149aef01d7175e34b36d8e96c79290a75ea4c
nesherson <nesad.omeragich27@gmail.com>
2021-10-27 19:47:17 +0200

    Update index view model

--- Appointify.Admin/ViewModels/Users/IndexViewModel.cs
+++ Appointify.Admin/ViewModels/Users/IndexViewModel.cs
@@ -6,6 +6,11 @@ namespace Appointify.Admin.ViewModels.Users
 {
     public class IndexViewModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; } 
+        public string Email { get; set; }
+
         public List<User> Users;
     }
 }
