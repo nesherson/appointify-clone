3964780b66354d2c0a51efa85ef7dcb85e8791a3
nesherson <nesad.omeragich27@gmail.com>
2021-10-27 19:47:25 +0200

    Add view model for edit

---
+++ Appointify.Admin/ViewModels/Users/EditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Appointify.Data.Entities;
+
+namespace Appointify.Admin.ViewModels.Users
+{
+    public class EditViewModel
+    { 
+      public int Id { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string Username { get; set; }
+      public string Email { get; set; }
+      public Role Role { get; set; }
+      public int CityId { get; set; }
+      public int CompanyId { get; set; }
+    }
+}
