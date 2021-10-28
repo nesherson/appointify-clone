ad0abd02e69c9c8e100dc853be876af0d49a2994
nesherson <nesad.omeragich27@gmail.com>
2021-10-28 20:04:47 +0200

    Export js functionality to separate file

---
+++ Appointify.Admin/wwwroot/js/GetCompanies.js
@@ -0,0 +1,61 @@
+﻿
+
+$(document).ready(function () {
+
+    var role = $('#roleId').val();
+
+    if (role == 1) {
+        getCompanyList();
+        $('#companyListId').removeClass('d-none');
+    } else {
+        $('#companyListId').addClass('d-none');
+    }
+
+
+    $('#roleId').change(function (e) {
+        var role = $('#roleId').val();
+        console.log("Role: ", role);
+
+        if (role == 1) {
+            getCompanyList();
+            $('#companyListId').removeClass('d-none');
+        } else {
+            $('#companyListId').addClass('d-none');
+        }
+    });
+
+
+});
+
+
+
+
+
+
+function getCompanyList() {
+    $.get("/users/getCompanies", function (data) {
+        $('#companyId').find('option').remove();
+        $(data).each(function (index, item) {
+            $('#companyId').append(
+                '<option value="' + item.id + '">' + item.name + '</option>'
+            );
+        });
+    });
+}
+
+
+
+//$.ajax({
+//    url: '@Url.Action("GetCompanies","Users")',
+//    type: 'GET',
+//    success: function (data) {
+//        $('#companyId').find('option').remove();
+//        $(data).each(function (index, item) {
+//            $('#companyId').append(
+//                '<option value="' + item.id + '">' + item.name + '</option>'
+//            );
+//        });
+//    },
+//    error: function () { },
+//});
+//  };
